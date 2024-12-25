ade925955086ee2fd00fc401b5fd2edeb1301531
Mmmustapha42 <111079800+Mmmustapha42@users.noreply.github.com>
2024-12-25 12:47:47 +0100

    nav&& mobilebar

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from "next/image"
+import { Separator } from "../ui/separator"
+import NavItems from "./NavItems"
+  
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <nav className="md:hidden">
+        <Sheet>
+            <SheetTrigger className="allign-middle"><Image src="/assets/icons/menu.svg" width={24} height={24} alt="menu"/></SheetTrigger>
+            <SheetContent className="flex flex-col bg-white gap-6 md:hidden">
+                <Image src='/assets/images/logo.svg' width={128} height={38} alt="logo"/>
+                <Separator className="border border-gray-50"/>
+                <NavItems/>
+            </SheetContent>
+        </Sheet>
+
+    </nav>
   )
 }
 
--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client"
+
+import { headerLinks } from '@/constants'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const NavItems = () => {
+    const pathname = usePathname()
   return (
-    <div>NavItems</div>
-  )
+    <ul className='md:flex-between flex w-full flex-col items-start gap-5 md:flex-row'>
+       {headerLinks.map((item)=>{
+        const isActive = pathname === item.route
+        return (
+            <li key={item.route}
+            className={`${isActive && 'text-primary-500'} flex-center p-medium-16 whitespace-nowrap`}
+            >
+                {item.label}
+            </li>
+        )
+       })}
+    </ul>
+)
 }
 
 export default NavItems
